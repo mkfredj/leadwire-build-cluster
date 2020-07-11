bce28fac78f0531b734d460e8a006620f6e76bda
Lead Wire <wassimdhib@yahoo.fr>
2020-07-11 19:07:37 +0200

    Update portail-install.sh

--- requirements/script/portail-install.sh
+++ requirements/script/portail-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-version=1.3.9b3
+version=${VERSION_LEADWIRE}
 curl -LJO  -H 'Authorization: token bc9ef0f060b991031b7208211b1767d5df10da16' https://github.com/leadwire-apm/leadwire-portail/archive/v${version}.tar.gz
 tar -xzvf leadwire-portail-${version}.tar.gz -C /usr/share/
 rm -f leadwire-portail-${version}.tar.gz
