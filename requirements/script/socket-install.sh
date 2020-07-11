a60c26aa99f8a75d187b4b6a6f0cf07373d40d20
Lead Wire <wassimdhib@yahoo.fr>
2020-07-11 22:49:45 +0200

    Update socket-install.sh

--- requirements/script/socket-install.sh
+++ requirements/script/socket-install.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-version=1.3.9
-curl -LJO  -H 'Authorization: token bc9ef0f060b991031b7208211b1767d5df10da16' https://github.com/leadwire-apm/leadwire-portail/archive/v${version}.tar.gz
-tar -xzvf leadwire-portail-${version}.tar.gz -C /usr/share/
-rm -f leadwire-portail-${version}.tar.gz
-mv /usr/share/leadwire-portail-${version} /usr/share/leadwire-portail
+version=${VERSION_PORTAIL}
+git clone --depth=50 --branch=evol/lot3 https://leadwire-apm:${GIT_TOKEN}@github.com/leadwire-apm/leadwire-portail.git /usr/share/leadwire-portail
+
 cd /usr/share/leadwire-portail
 npm install
 npm install -g forever
