dee238e58c7e66f43dd8e8a4dc8eed1fd5badb79
WassimDhib <wassimdhib@yahoo.fr>
2020-07-13 20:28:20 +0000

    fix portail build

--- requirements/script/portail-entrypoint.sh
+++ requirements/script/portail-entrypoint.sh
@@ -9,3 +9,12 @@ cd /usr/share/leadwire-portail/
 sudo -u nginx bin/console assets:install --env=prod
 sudo -u nginx bin/console assetic:dump --env=prod
 sudo -u nginx bin/console cache:clear --env=prod
+
+CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
+    touch $CONTAINER_ALREADY_STARTED
+    echo "-- First container startup --"
+    sudo -u nginx bin/console leadwire:install --env=prod
+else
+    echo "-- Not first container startup --"
+fi
