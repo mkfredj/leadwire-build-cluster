c78c2704670cbde9c3d8399aa9f0092eeba09119
root <root@vps-e716918b.vps.ovh.net.novalocal>
2020-10-29 22:23:45 +0000

    1.0.4

--- requirements/script/portail-install.sh
+++ requirements/script/portail-install.sh
@@ -4,7 +4,7 @@ cd /usr/share/leadwire-portail
 #sudo -u nginx sh /usr/share/leadwire-portail/get_composer.sh
 
 sudo -u nginx php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-sudo -u nginx php composer-setup.php
+sudo -u nginx php composer-setup.php --version=1.10.15
 sudo -u nginx php -r "unlink('composer-setup.php');"
 
 sudo -u nginx php /usr/share/leadwire-portail/composer.phar install --no-dev --no-scripts --optimize-autoloader
